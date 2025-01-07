f89768ff89e78e4cc5e2ca049ff9c1730762e99e
minb__m <kmb3322@naver.com>
2025-01-07 12:02:01 +0900

    health추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './modules/users/users.module';
-import { TripsModule } from './modules/trips/trips.module';
-import { ImageMetadataModule } from './modules/imagemetadata/image-metadata.module';
-import { UploadModule } from './modules/uploadImage/upload.module';
-import { AuthModule } from './auth/auth.module';
-import { GoogleUserModule } from './modules/google-user/google-user.module';
 import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
 import { GoogleStrategy } from './auth/google.strategy';
+import { HealthModule } from './health/health.module';
+import { GoogleUserModule } from './modules/google-user/google-user.module';
 import { GoogleUser, GoogleUserSchema } from './modules/google-user/schemas/google-user.schema';
+import { ImageMetadataModule } from './modules/imagemetadata/image-metadata.module';
+import { TripsModule } from './modules/trips/trips.module';
+import { UploadModule } from './modules/uploadImage/upload.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -29,7 +30,7 @@ import { GoogleUser, GoogleUserSchema } from './modules/google-user/schemas/goog
     MongooseModule.forFeature([
       { name: GoogleUser.name, schema: GoogleUserSchema }
     ]),
-    
+    HealthModule,
     AuthModule,
     UsersModule,
     TripsModule,
